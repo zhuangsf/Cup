e652efd9ec8b15f4f3df477f0fb8a4424c852ba6
zhuangsf <423589402@qq.com>
2015-11-04 00:00:18 +0800

    水温界面 修改

--- src/com/sf/cup/FragmentWater.java
+++ src/com/sf/cup/FragmentWater.java
@@ -1,57 +1,160 @@
 package com.sf.cup;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import com.sf.cup.utils.Utils;
+
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class FragmentWater extends Fragment {
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
- 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_water, null);
-    }
- 
-    public static FragmentWater newInstance(Bundle b){
-    	FragmentWater fd=new FragmentWater();
-			fd.setArguments(b);
-			return fd;
+
+	ListView temperatureListView;
+	Button add_temperature_button;
+	List<Map<String, Object>> temperatureList = new ArrayList<Map<String, Object>>();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.tab_water, null);
+
+		temperatureListView = (ListView) v.findViewById(R.id.temperature_list);
+		TemperatureListViewAdapter hlva = new TemperatureListViewAdapter(this.getActivity(), getData(),
+				R.layout.tab_water_select_item, new String[] { "info_text", "temperature_text", "radio_btn" },
+				new int[] { R.id.info_text, R.id.temperature_text, R.id.radio_btn });
+		temperatureListView.setAdapter(hlva);
+		setHeight(hlva, temperatureListView);
+
+		add_temperature_button = (Button) v.findViewById(R.id.add_temperature_button);
+		add_temperature_button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				LayoutInflater inflater = getActivity().getLayoutInflater();
+				final View layout = inflater.inflate(R.layout.tab_water_select_dialog,
+						(ViewGroup) v.findViewById(R.id.dialog));
+				AlertDialog   ad = new AlertDialog.Builder(getActivity()).setPositiveButton("确定", null).setNegativeButton("取消", null).create();
+				ad.setTitle("温度模式设定");
+				ad.setView(layout);
+				ad.show();
+				//TODO 这里有待验证是否能正确弹出输入窗口
+				layout.post(new Runnable() {
+			        @Override
+			        public void run() {
+			            InputMethodManager im = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+			            im.showSoftInput(layout, InputMethodManager.SHOW_FORCED);
+			        }
+			    });
+//				ad.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM); 
+//				ad.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+//				ad.getWindow().setContentView(R.layout.tab_water_select_dialog);
+			}
+		});
+
+		return v;
+	}
+
+	public static FragmentWater newInstance(Bundle b) {
+		FragmentWater fd = new FragmentWater();
+		fd.setArguments(b);
+		return fd;
+	}
+
+	public void setHeight(BaseAdapter comAdapter, ListView l) {
+		int listViewHeight = 0;
+		int adaptCount = comAdapter.getCount();
+		Utils.Log("xxxxxxxxxxxxxxxxxx adaptCount:" + adaptCount);
+		for (int i = 0; i < adaptCount; i++) {
+			View temp = comAdapter.getView(i, null, l);
+			temp.measure(0, 0);
+			listViewHeight += temp.getMeasuredHeight();
+			Utils.Log("xxxxxxxxxxxxxxxxxx listViewHeight:" + listViewHeight);
 		}
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /**
-   	 * to avoid IllegalStateException: No activity
-   	 */
-   	@Override
-   	public void onDetach() {
-   	    super.onDetach();
-   	    try {
-   	        Field childFragmentManager = Fragment.class.getDeclaredField("mChildFragmentManager");
-   	        childFragmentManager.setAccessible(true);
-   	        childFragmentManager.set(this, null);
+		LayoutParams layoutParams = l.getLayoutParams();
+		layoutParams.width = LayoutParams.FILL_PARENT;
+		layoutParams.height = listViewHeight + 2;
+		l.setLayoutParams(layoutParams);
+	}
 
-   	    } catch (NoSuchFieldException e) {
-   	        throw new RuntimeException(e);
-   	    } catch (IllegalAccessException e) {
-   	        throw new RuntimeException(e);
-   	    }
+	private class TemperatureListViewAdapter extends SimpleAdapter {
 
-   	}
+		public TemperatureListViewAdapter(Context context, List<Map<String, Object>> data, int resource, String[] from,
+				int[] to) {
+			super(context, data, resource, from, to);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = super.getView(position, convertView, parent);
+			view.setOnClickListener(new MyListener(position));
+			return view;
+		}
+
+	}
+
+	private class MyListener implements OnClickListener {
+		int mPosition;
+
+		public MyListener(int inPosition) {
+			mPosition = inPosition;
+		}
+
+		@Override
+		public void onClick(View v) {
+
+		}
+
+	}
+
+	private List<Map<String, Object>> getData() {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		Map<String, Object> map;
+		for (int i = 0; i < temperatureList.size(); i++) {
+			map = new HashMap<String, Object>();
+			map.put("info_text", temperatureList.get(i).get("info_text"));
+			map.put("temperature_text", temperatureList.get(i).get("temperature_text"));
+			map.put("radio_btn", "false");
+			list.add(map);
+		}
+
+		return list;
+	}
+
+	/**
+	 * to avoid IllegalStateException: No activity
+	 */
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		try {
+			Field childFragmentManager = Fragment.class.getDeclaredField("mChildFragmentManager");
+			childFragmentManager.setAccessible(true);
+			childFragmentManager.set(this, null);
+
+		} catch (NoSuchFieldException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
 }

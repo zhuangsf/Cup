fb2faa89e63064de63f988e54cccd40cd5f87386
zhuangsf <423589402@qq.com>
2015-11-18 23:04:56 +0800

    闹钟提示的 处理好

--- src/com/sf/cup/MainActivity.java
+++ src/com/sf/cup/MainActivity.java
@@ -1,14 +1,20 @@
 package com.sf.cup;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.sf.cup.utils.Utils;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -52,6 +58,11 @@ public class MainActivity extends Activity {
 			};
 
 
+	
+	
+	private MediaPlayer mp;  
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -69,6 +80,13 @@ public class MainActivity extends Activity {
 			Utils.Log("xxxxxxxxxxxxxxxxxx onCreate home:"+fWater);
 			getFragmentManager().beginTransaction().show(fWater).commit();
 		}
+		
+		
+		// if from alarm show the dialog
+		startFromAlarm(getIntent());
+		
+		
+	
 	}
 
 	private void createFragment() {
@@ -147,6 +165,76 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
+	
+	
+	
+	
+//	onNewIntent(Intent intent)方法里调用setIntent(intent)设置这个传来的最新的intent.
+	
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		setIntent(intent);
+		startFromAlarm(intent);
+	}
+	
+	private void startFromAlarm(Intent intent){
+		boolean isAlarm=intent.getBooleanExtra(Utils.IS_FROM_ALARM, false);
+		int index=intent.getIntExtra(Utils.FROM_ALARM_INDEX,-1);
+		Utils.Log("isAlarm:"+ isAlarm+" index:"+index);
+		if(isAlarm){
+		//如果是闹钟响起跳转来的，播个音乐
+		 // 初始化音乐资源  
+       try {  
+       	
+       	new AlertDialog.Builder(this)
+			.setMessage("亲！已到设定饮水时间咯！\n请及时享用哦")
+			.setPositiveButton("确定", null)
+			.create()
+			.show();
+       	
+           // 创建MediaPlayer对象  
+           mp = new MediaPlayer();  
+           // 将音乐保存在res/raw/xingshu.mp3,R.java中自动生成{public static final int xingshu=0x7f040000;}  
+           Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);  
+//           mp = MediaPlayer.create(this, notification);  
+           mp.setDataSource(this,notification);
+           // 在MediaPlayer取得播放资源与stop()之后要准备PlayBack的状态前一定要使用MediaPlayer.prepeare()  
+           mp.prepare();  
+           // 开始播放音乐  
+           mp.start();  
+           // 音乐播放完毕的事件处理  
+           mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {  
+               public void onCompletion(MediaPlayer mp) {  
+                   // 循环播放  
+                   try {  
+//                       mp.start();  
+                   } catch (IllegalStateException e) {  
+                       e.printStackTrace();  
+                   }  
+               }  
+           });  
+           // 播放音乐时发生错误的事件处理  
+           mp.setOnErrorListener(new MediaPlayer.OnErrorListener() {  
+               public boolean onError(MediaPlayer mp, int what, int extra) {  
+                   // 释放资源  
+                   try {  
+                       mp.release();  
+                   } catch (Exception e) {  
+                       e.printStackTrace();  
+                   }  
+                   return false;  
+               }  
+           });  
+       } catch (IllegalStateException e) {  
+           e.printStackTrace();  
+       } catch (IOException e) {  
+           e.printStackTrace();  
+       }  
+		}
+	}
+	
+	
 	
 	
 	public void onBackPressed()
